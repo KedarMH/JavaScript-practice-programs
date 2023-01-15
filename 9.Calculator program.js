7db6ac85d60e35212bde811fb3afef5d3e411ffc
KedarMH <kedarmh19@gmail.com>
2023-01-15 17:02:21 +0530

    9th code added

---
+++ 9.Calculator program.js
@@ -0,0 +1,33 @@
+//  Simple calculator program
+
+let result;
+
+// operator input
+const operator = prompt('Enter operator (+,-,* or /):');
+
+//operands input
+const num1 = parseFloat(prompt('Enter first number: '));
+const num2 = parseFloat(prompt('Enter second number: '));
+
+switch (operator) {
+    case '+':
+        result = num1 + num2;
+        console.log(`${num1} + ${num2} = ${result}`);
+        break;
+    case '-':
+        result = num1 - num2;
+        console.log(`${num1} - ${num2} = ${result}`);
+        break;
+    case '*':
+        result = num1 * num2;
+        console.log(`${num1} * ${num2} = ${result}`);
+        break;
+    case '/':
+        result = num1 / num2;
+        console.log(`${num1} / ${num2} = ${result}`);
+        break;
+    default:
+        console.log('Invalid Operator');
+        break;
+
+}
