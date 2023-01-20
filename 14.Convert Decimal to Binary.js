3b51842304931bf39a3602a9576f9a0c5fee3722
KedarMH <kedarmh19@gmail.com>
2023-01-20 18:59:22 +0530

    added 14th program

---
+++ 14.Convert Decimal to Binary.js
@@ -0,0 +1,7 @@
+// Convert Decimal to Binary using toString()
+const number = 9;
+const bin = number.toString(2);
+console.log(`Binary of ${number} is ${bin}`);
+
+//toString([radix]) returns a string value in a specified radix (base).
+//Here, toString(2) converts the decimal number to binary number.
