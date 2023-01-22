dde2a24b6c73c4010a5ad451f1bda6ab1236970c
KedarMH <kedarmh19@gmail.com>
2023-01-22 23:06:04 +0530

    updated

--- 16. Convert time format to 24hrs.js
+++ 16. Convert time format to 24hrs.js
@@ -1,17 +1,18 @@
+// Program to covert time from 12 hour format to 24 hours format
 
 const time = '12:10AM';
 
 function convertTo24HrsFormat(timeText) {
-  var timeTextLower = timeText.toLowerCase();
-  let [hours, mins] = timeTextLower.split(":");
+      var timeTextLower = timeText.toLowerCase();
+      let [hours, mins] = timeTextLower.split(":");
 
-  // 12 o clock is the special case to be handled both for AM and PM
-  if (timeTextLower.endsWith("am"))
-        hours = hours == 12 ? "0" : hours;
-  else if (timeTextLower.endsWith("pm"))
-        hours = hours == 12 ? hours : String(+hours + 12);
+      // 12 o clock is the special case to be handled both for AM and PM
+      if (timeTextLower.endsWith("am"))
+            hours = hours == 12 ? "0" : hours;
+      else if (timeTextLower.endsWith("pm"))
+            hours = hours == 12 ? hours : String(+hours + 12);
 
-  return hours.padStart(2, 0) + ":" + mins.slice(0, -2).padStart(2, 0);
+      return hours.padStart(2, 0) + ":" + mins.slice(0, -2).padStart(2, 0);
 }
 
 console.log(`Converted time: ${convertTo24HrsFormat(time)}`)
