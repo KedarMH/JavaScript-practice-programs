4ecf61babe4b279320cbd95d938626d972f8f1bb
KedarMH <kedarmh19@gmail.com>
2023-03-22 09:57:16 +0530

    Added API fetching

--- API calling/script.js
+++ API calling/script.js
@@ -1,4 +1,21 @@
 const button = document.getElementById("search-button")
-button.addEventListener("click", () =>{
-    
+const input = document.getElementById("city-input")
+
+const cityName = document.getElementById('city-name')
+const cityTime = document.getElementById('city-time')
+const cityTemp = document.getElementById('city-temp')
+
+async function getData(cityName) {
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=3908e01308244c3b8cd140854231403&q=${cityName}&aqi=yes`)
+    return await promise.json()
+}
+
+button.addEventListener("click", async () => {
+    const value = input.value;
+    const result = await getData(value);
+    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`
+    cityTime.innerText = `${result.location.localtime}`
+    cityTemp.innerText = `${result.current.temp_c}`
 })
+
+// http://api.weatherapi.com/v1/current.json?key=3908e01308244c3b8cd140854231403&q=London&aqi=yes
